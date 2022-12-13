acf45716caa97cd89b54478bf29ff6097ca42abc
Rojas-Andres <andresfelipe200004@gmail.com>
2022-12-13 18:54:15 -0500

    implementacion metodo post

--- modules/users/app.py
+++ modules/users/app.py
@@ -6,23 +6,17 @@ from db.database import DataBase
 def lambda_handler(event, context):
     print("Received event: ", event)
     print("Context: ", context)
-
-    db = DataBase()
-    users = db.get_users()
-    list_users = []
-    for i in users:
-        list_users.append(
-            {
-                "id": i[0],
-                "nombre": i[1],
-                "apellido": i[2],
-                "ciudad": i[3],
-            }
-        )
+    data = {}
+    if event.get("path") == "/users" and event.get("httpMethod") == "GET":
+        db = DataBase()
+        users = db.get_users()
+        data = {"users": [dict(user) for user in users]}
+    elif event.get("path") == "/users" and event.get("httpMethod") == "POST":
+        data = {"message": "CREATE USER"}
     return {
         "headers": {
             "Access-Control-Allow-Origin": "*",
         },
         "statusCode": 200,
-        "body": json.dumps({"users": list_users}),
+        "body": json.dumps(data),
     }
